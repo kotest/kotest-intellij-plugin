b57b2949b106026439935ccb3cbe578fe2139881
Emil Kantis <emil.kantis@protonmail.com>
2021-12-15 21:26:50 +0100

    Feedback fixes

--- src/main/kotlin/io/kotest/plugin/intellij/inspections/ShouldBeWithUnequalTypesInspection.kt
+++ src/main/kotlin/io/kotest/plugin/intellij/inspections/ShouldBeWithUnequalTypesInspection.kt
@@ -4,14 +4,13 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import io.kotest.plugin.intellij.psi.TypeSet
+import io.kotest.plugin.intellij.psi.isComparableTo
 import io.kotest.plugin.intellij.psi.tryResolveType
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.psi.KtBinaryExpression
-import org.jetbrains.kotlin.types.KotlinType
-import org.jetbrains.kotlin.types.checker.NewKotlinTypeChecker
 
 class ShouldBeWithUnequalTypesInspection : AbstractKotlinInspection() {
+   val testedOperations = listOf("shouldBe", "shouldNotBe")
 
    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
       return object : PsiElementVisitor() {
@@ -21,58 +20,16 @@ class ShouldBeWithUnequalTypesInspection : AbstractKotlinInspection() {
                   val lhsType = element.left?.tryResolveType()
                   val rhsType = element.right?.tryResolveType()
 
-                  when (lhsType isComparableTo rhsType) {
-                     true -> Unit // Let test compare the types
-                     false -> {
-                        holder.registerProblem(
-                           element,
-                           "Comparing incompatible types $lhsType and $rhsType",
-                           ProblemHighlightType.WARNING,
-                        )
-                     }
+                  if (!lhsType.isComparableTo(rhsType)) {
+                     holder.registerProblem(
+                        element,
+                        "Comparing incompatible types $lhsType and $rhsType",
+                        ProblemHighlightType.WARNING,
+                     )
                   }
                }
             }
          }
       }
-   }
-
-   companion object {
-      val testedOperations = listOf("shouldBe", "shouldNotBe")
-
-      private val comparableTypes = setOf(
-         TypeSet("Int", "Long"),
-         TypeSet("IntArray", "LongArray"),
-         TypeSet("Float", "Double"),
-         TypeSet("FloatArray", "DoubleArray"),
-      )
-
-      /**
-       * Determines whether two types are considered comparable.
-       * Everything is considered comparable with a Matcher
-       * Otherwise, if LHS <:< RHS or RHS <:< LHS (where <:< means subtype or type), we consider them comparable
-       * We can also manually mark types as comparable using [comparableTypes].
-       */
-      infix fun KotlinType?.isComparableTo(rhs: KotlinType?): Boolean =
-         when {
-            this == null && rhs == null -> true
-            this != null && rhs != null ->
-               rhs.isMatcher() ||
-                  NewKotlinTypeChecker.Default.equalTypes(this, rhs) ||
-                  NewKotlinTypeChecker.Default.isSubtypeOf(this, rhs) ||
-                  NewKotlinTypeChecker.Default.isSubtypeOf(rhs, this) ||
-                  comparableTypes.any { it.contains(this, rhs) }
-            else -> false
-         }
-
-      // This requires the rhs to actually be of a type named "*Matcher*"
-      //
-      // Using `TypeUtils.getAllSupertypes` for an object implementing `Matcher` only gives `Any` as supertype for
-      // some reason.
-      //
-      // Perhaps we can add annotations to all implementations of `Matcher<*>` using a compiler plugin? That could
-      // be picked up here later
-      private fun KotlinType.isMatcher() =
-         toString().contains("Matcher")
    }
 }
--- src/main/kotlin/io/kotest/plugin/intellij/psi/types.kt
+++ src/main/kotlin/io/kotest/plugin/intellij/psi/types.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.checker.NewKotlinTypeChecker
 import org.jetbrains.kotlin.types.typeUtil.makeNotNullable
 
 /**
@@ -27,3 +28,41 @@ internal class TypeSet(vararg typeNames: String) {
    fun contains(vararg type: KotlinType) =
       types.containsAll(type.map { it.toString() })
 }
+
+private val defaultComparableTypes = setOf(
+   TypeSet("Int", "Long"),
+   TypeSet("IntArray", "LongArray"),
+   TypeSet("Float", "Double"),
+   TypeSet("FloatArray", "DoubleArray"),
+)
+
+/**
+ * Determines whether two types are considered comparable.
+ * Everything is considered comparable with a Matcher
+ * Otherwise, if LHS <:< RHS or RHS <:< LHS (where <:< means subtype or type), we consider them comparable
+ * We can also manually mark types as comparable using [comparableTypes].
+ */
+internal fun KotlinType?.isComparableTo(
+   rhs: KotlinType?,
+   comparableTypes: Set<TypeSet> = defaultComparableTypes
+): Boolean =
+   when {
+      this == null && rhs == null -> true
+      this != null && rhs != null ->
+         rhs.isMatcher() ||
+            NewKotlinTypeChecker.Default.equalTypes(this, rhs) ||
+            NewKotlinTypeChecker.Default.isSubtypeOf(this, rhs) ||
+            NewKotlinTypeChecker.Default.isSubtypeOf(rhs, this) ||
+            comparableTypes.any { it.contains(this, rhs) }
+      else -> false
+   }
+
+// This requires the rhs to actually be of a type named "*Matcher*"
+//
+// Using `TypeUtils.getAllSupertypes` for an object implementing `Matcher` only gives `Any` as supertype for
+// some reason.
+//
+// Perhaps we can add annotations to all implementations of `Matcher<*>` using a compiler plugin? That could
+// be picked up here later
+private fun KotlinType.isMatcher() =
+   toString().contains("Matcher")
