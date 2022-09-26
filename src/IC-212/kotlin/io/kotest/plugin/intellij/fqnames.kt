e44edb25b842357b21c164cea78f10169448bcdd
sksamuel <sam@sksamuel.com>
2022-09-26 06:50:18 -0500

    Fix deprecations in latest releases

--- src/IC-212/kotlin/io/kotest/plugin/intellij/fqnames.kt
+++ src/IC-212/kotlin/io/kotest/plugin/intellij/fqnames.kt
@@ -1,6 +1,6 @@
 package io.kotest.plugin.intellij
 
-import org.jetbrains.kotlin.idea.search.getKotlinFqName
+import org.jetbrains.kotlin.idea.refactoring.fqName.getKotlinFqName
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtClassOrObject
 
