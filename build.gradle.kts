d3207101e8b095c8882b7a76bfd237c3aabff3c7
sksamuel <sam@sksamuel.com>
2024-05-27 09:05:07 -0500

    runtimeOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,7 +151,7 @@ dependencies {
    // needed for the resource files which are loaded into java light tests
    testImplementation(libs.test.kotest.framework.api)
    testImplementation(libs.test.kotest.assertions.core)
-   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+   runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 sourceSets {
