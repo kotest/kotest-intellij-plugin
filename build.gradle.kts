cfedcfc2b1d9de059d9a94d6ff093e82b169f97e
sksamuel <sam@sksamuel.com>
2024-05-27 08:05:50 -0500

    Adding support for IC-242

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,9 +116,9 @@ val plugins = listOf(
       deps = listOf("java", "org.jetbrains.plugins.gradle", "org.jetbrains.kotlin")
    ),
    PluginDescriptor(
-      since = "242.12881.48", // this version is 2024.2
+      since = "242.10180.25", // this version is 2024.2
       until = "242.*",
-      sdkVersion = "242.12881-EAP-CANDIDATE-SNAPSHOT",
+      sdkVersion = "242.10180-EAP-CANDIDATE-SNAPSHOT",
       sourceFolder = "IC-242",
       deps = listOf("java", "org.jetbrains.plugins.gradle", "org.jetbrains.kotlin")
    ),
