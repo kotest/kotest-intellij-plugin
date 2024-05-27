005949c4c02e4dc01e0cecdbd8f66244457794d1
sksamuel <sam@sksamuel.com>
2024-05-27 08:59:47 -0500

    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")

--- build.gradle.kts
+++ build.gradle.kts
@@ -151,6 +151,7 @@ dependencies {
    // needed for the resource files which are loaded into java light tests
    testImplementation(libs.test.kotest.framework.api)
    testImplementation(libs.test.kotest.assertions.core)
+   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 sourceSets {
