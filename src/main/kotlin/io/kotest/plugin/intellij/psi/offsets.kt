008e2f3e2f4a4b390b0c89085cb15348f21ae700
Risalat Zaman <1000000death@gmail.com>
2024-09-14 05:11:33 +0600

    Fixed navigation issues

--- src/main/kotlin/io/kotest/plugin/intellij/psi/offsets.kt
+++ src/main/kotlin/io/kotest/plugin/intellij/psi/offsets.kt
@@ -4,6 +4,7 @@ import com.intellij.execution.PsiLocation
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtilCore
 
 /**
  * Returns the offsets for the given line in this file, or -1 if the document cannot be loaded for this file.
@@ -19,43 +20,22 @@ fun PsiFile.offsetForLine(line: Int): IntRange? {
    }
 }
 
-fun PsiFile.offsetForTestLine(line: Int): Int? {
-   val doc = PsiDocumentManager.getInstance(project).getDocument(this) ?: return null
-   return try {
-      doc.getLineStartOffset(line)
-   } catch (e: Exception) {
-      null
-   }
-}
-
-fun PsiFile.findTestElementAtLine(line: Int): PsiElement?  {
-   return offsetForTestLine(line)?.let { findElementAt(it) }
-}
-
 /**
  * Finds the first [PsiElement] for the given offset range by iterating over
- * the values in the range until an element is found.
+ * the values in the range until an element that is not a whitespace is found.
  */
 fun PsiElement.findElementInRange(offsets: IntRange): PsiElement? {
    return offsets.asSequence()
-      .mapNotNull { findElementAt(it) }
-      .elementAtOrNull(1)
+      .mapNotNull { offset -> findElementAt(offset)?.takeIf { it.text?.isNotBlank() == true } }
+      .firstOrNull()
 }
-
 /**
  * Returns the first element for the given line in this file,
  * or null if the document cannot be loaded for this file.
  *
  * Note: This method is 1 indexed.
  */
-fun PsiFile.elementAtLine(line: Int): PsiElement?  {
-   if (line < 1) return null
-
-   return when (line) {
-      1 -> offsetForLine(line)?.let { findElementInRange(it) }
-      else -> findTestElementAtLine(line)
-   }
-}
-
+fun PsiFile.elementAtLine(line: Int): PsiElement? =
+   offsetForLine(line)?.let { findElementInRange(it) }
 
 fun PsiElement.toPsiLocation() = PsiLocation(project, this)
